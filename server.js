6e46666e459c543e8d8de3904c6b54841a6877cd
Dadis <raditztv@gmail.com>
2025-04-01 23:41:49 +0200

    search user

--- server.js
+++ server.js
@@ -22,10 +22,32 @@ app.get('/api/inventory/:steamID', async (req, res) => {
     if (!data || data.error) {
       return res.status(404).json({ error: "Inventaire introuvable ou Steam ID invalide" });
     }
-    
+
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: "Erreur lors de la récupération de l'inventaire" });
+    console.error("Erreur serveur:", error);
+  }
+});
+
+app.get('/api/searchUser/:search', async (req, res) => {
+  const name = req.params.search;
+
+  if (!name) {
+    return res.status(400).json({ error: "Le nom est requis" });
+  }
+
+  try {
+    const response = await fetch(`https://www.steamwebapi.com/explore/api/profile?key=${process.env.VITE_APP_STEAMWEB_API_KEY}&name=${name}`);
+    const data = await response.json();
+
+    if (!data || data.error) {
+      return res.status(404).json({ error: "Utilisateur introuvable" });
+    }
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la recherche de l'utilisateur" });
     console.error("Erreur serveur:", error);
   }
 });
